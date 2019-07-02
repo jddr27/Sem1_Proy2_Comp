c9cb7e57d274b5ce362903a32222fbad83161ad8
jddr27 <jddeleonr.inf2013@gmail.com>
2019-07-01 22:11:58 -0600

    com1

--- database-service/startDatabase.sh
+++
@@ -1,6 +0,0 @@
-docker run --name some-mysql \
--e MYSQL_ROOT_PASSWORD=123456789 \
--d my-mysql \
---character-set-server=utf8mb4 \
---collation-server=utf8mb4_unicode_ci \
---default-authentication-plugin=mysql_native_password
